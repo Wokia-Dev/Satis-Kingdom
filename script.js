e1c45fafe6c03ea623a6e83c89afac253deeeaa4
Wokia-Dev <quentin.ruhf@gmail.com>
2025-01-12 12:36:06 +0100

    SlideShow

---
+++ script.js
@@ -0,0 +1,28 @@
+﻿let slidesIndex = 1;
+showSlides(slidesIndex);
+
+function changeSlide(n) {
+    showSlides(slidesIndex += n);
+}
+
+function setSlide(n) {
+    showSlides(slidesIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slidesIndex = 1; }
+    if (n < 1) { slidesIndex = slides.length; }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    if (slides.length > 0 && dots.length > 0) {
+        slides[slidesIndex - 1].style.display = "block";
+        dots[slidesIndex - 1].className += " active";
+    }
+}
