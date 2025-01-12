0cd2a5ca6ee641697e360e22f555b0cd0f30299a
Wokia-Dev <quentin.ruhf@gmail.com>
2025-01-12 14:01:21 +0100

    Lorem Ipsum

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 ﻿let slidesIndex = 1;
 const titles = ["Title 1", "Title 2", "Title 3"];
-const descriptions = [
-    "This is the description for the first slide. It explains the first aspect of the game.",
-    "This is the description for the second slide. It provides details on the second feature.",
-    "This is the description for the third slide. It highlights the third key element."
-];
+const descriptions = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit tortor quis aliquam pulvinar. In cursus tincidunt magna, eget feugiat velit tincidunt sit amet. Nunc molestie tellus eget urna bibendum, ac efficitur urna pretium. Mauris eu posuere elit. Nunc ac sollicitudin dui, sit amet feugiat risus. Quisque convallis sagittis velit sit amet facilisis. Donec viverra ante suscipit, aliquet ligula et, congue eros. Mauris cursus pharetra fermentum. Ut sodales leo ex. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam ut ultricies dolor, sit amet ullamcorper quam. Nam sed varius neque.",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quam ex, elementum eget congue sit amet, malesuada sed lectus. Maecenas porta aliquet dui vel lacinia. Suspendisse ornare libero ligula, et tincidunt risus varius et. Nunc sapien erat, mollis ut bibendum nec, placerat et ex. Vivamus faucibus ex id pretium gravida. Nam fringilla vehicula scelerisque. Donec et luctus nibh. Suspendisse interdum est vitae urna egestas, vitae aliquet dolor mollis. In id elit ut turpis egestas hendrerit. Integer id tortor leo. Nam a varius dolor, vitae auctor massa. Quisque rhoncus sagittis lobortis. Donec viverra justo ac tristique condimentum. Vestibulum nec hendrerit ex. Curabitur eget tellus ipsum. Aenean finibus mauris non arcu aliquam, at placerat lacus malesuada.",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae libero ex. Cras a volutpat ex. Nulla facilisi. Curabitur molestie mauris vel libero lobortis, ut sodales tellus lacinia. Donec in ultricies mi, vitae lacinia nibh. Curabitur ante sem, laoreet aliquet vulputate sit amet, laoreet id tellus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras eget augue et nisi ornare ultrices et quis augue. Etiam vel porttitor ipsum. Donec lacinia ligula vitae bibendum imperdiet. Praesent sit amet condimentum nibh. Nunc metus felis, rhoncus eu commodo sit amet, sagittis ut augue. Pellentesque elementum lectus eu quam posuere euismod."];
 
 let autoSlideInterval;
 let isMouseOver = false;
@@ -30,8 +28,12 @@ function showSlides(n) {
     let title = document.getElementById("slideTitle");
     let description = document.getElementById("slideDescription");
 
-    if (n > slides.length) { slidesIndex = 1; }
-    if (n < 1) { slidesIndex = slides.length; }
+    if (n > slides.length) {
+        slidesIndex = 1;
+    }
+    if (n < 1) {
+        slidesIndex = slides.length;
+    }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
